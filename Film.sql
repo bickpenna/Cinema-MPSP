3061b7a705168b45d64403ffb103f3d4aba55e21
Mario Penna <117465087+bickpenna@users.noreply.github.com>
2022-11-04 23:06:23 +0100

    I FILM
    Non capisco come si mettono più modifiche insieme, bha

---
+++ Film.sql
@@ -0,0 +1,50 @@
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Spartacus', 1960, 'Stanley Kubrick');
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Lolita', 1962, 'Stanley Kubrick');
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Il dottor Stranamore', 1964, 'Stanley Kubrick');
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Il buono, il brutto, il cattivo', 1966, 'Sergio Leone');
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('2001: Odissea nello spazio', 1968, 'Stanley Kubrick')
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Arancia meccanica', 1971, 'Stanley Kubrick')
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Shining', 1980, 'Stanley Kubrick')
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('E.T. l''extra-terrestre', 1982, 'Steven Spielberg');
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Amore Tossico', 1983, 'Claudio Caligari');
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Indiana Jones e il tempio maledetto', 1984, 'Steven Spielberg');
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Full Metal Jacket', 1987, 'Stanley Kubrick')
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Indiana Jones e l''ultima crociata ', 1989, 'Steven Spielberg');
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Hook - Capitan Uncino', 1991, 'Steven Spielberg');
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Jurassic Park', 1993, 'Steven Spielberg');
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Schindler''s List', 1993, 'Steven Spielberg');
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('L''odore della notte', 1998, 'Claudio Caligari');
+
+INSERT INTO cin.film(titolo, anno, regista)
+VALUES ('Non essere cattivo', 2015, 'Claudio Caligari');
