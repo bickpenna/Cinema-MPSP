a35c21215175c05dc281dc14e3dd06ea9e71ca6c
Mario Penna <117465087+bickpenna@users.noreply.github.com>
2022-11-04 22:14:40 +0100

    Cinema
    Brother ho aggiunto i cinema, tutte le info so corrette, le ho googlate

---
+++ Cinema.sql
@@ -0,0 +1,20 @@
+INSERT INTO cin.cinema(nome, indirizzocitta, numtelefono)
+VALUES ('Modernissimo', 'Via Cisterna dell''Olio, 49/59, 80134 Napoli NA', 0815800254);
+
+INSERT INTO cin.cinema(nome, indirizzocitta, numtelefono)
+VALUES ('UCI Cinemas Casoria', 'Via S. Salvatore, 1, 80026 Casoria NA', 892960);
+
+INSERT INTO cin.cinema(nome, indirizzocitta, numtelefono)
+VALUES ('UCI Cinemas Casoria', 'Via S. Salvatore, 1, 80026 Casoria NA', 892960);
+
+INSERT INTO cin.cinema(nome, indirizzocitta, numtelefono)
+VALUES ('UCI Cinemas Cinepolis Marcianise', 'Strada Statale 87, NC, 81025 Marcianise CE', 892960);
+
+INSERT INTO cin.cinema(nome, indirizzocitta, numtelefono)
+VALUES ('Metropolitan', 'Via Chiaia, 149, 80132 Napoli NA', 081415562);
+
+INSERT INTO cin.cinema(nome, indirizzocitta, numtelefono)
+VALUES ('Multisala Odeon Pisa', 'Piazza S. Paolo All''Orto, 18, 56127 Pisa PI', 050540168);
+
+INSERT INTO cin.cinema(nome, indirizzocitta, numtelefono)
+VALUES ('Cinema Teatro San Marco', 'Viale Aldo Moro, 26, 87067 Rossano CS', 0983292986);
