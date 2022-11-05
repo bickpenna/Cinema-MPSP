6a4d89442485c485b033ccaa73b3b60d2913f6b5
Simone Parente <107051698+ssimone-p@users.noreply.github.com>
2022-11-05 11:45:43 +0100

    Popolazione Table Sale
    Tutti i cinema da ID 1 a 7

---
+++ Sale.sql
@@ -0,0 +1,73 @@
+--Sale Cinema "Modernissimo" ID 1
+INSERT INTO cin.sala(numero, schermo, impiantoaudio, impiantovideo, dimensione, cinema)
+(
+    values  (1, 'Ignifugo Norma Europea EN13501-1', 'Sony', 'Sony SRX-R510DA', '250', 1),
+            (2, 'Ignifugo Norma Europea EN13501-1', 'Bose', 'Sony SRX-R510DB', '250', 1),
+            (3, 'Ignifugo Norma Europea EN13501-1', 'Yamaha', 'Sony SRX-R510DC', '250', 1),
+            (4, 'Ignifugo Norma Europea EN13501-1', 'Sony', 'Sony SRX-R510DD', '250', 1),
+            (5, 'Ignifugo Norma Europea EN13501-1', 'Sony', 'Sony SRX-R510DS', '250', 1)
+);
+
+--Sale Cinema "UCI Cinemas Casoria" ID 2
+INSERT INTO cin.sala(numero, schermo, impiantoaudio, impiantovideo, dimensione, cinema)
+(
+    values  (1, 'Ignifugo Norma Europea EN13501-1', 'Bose', 'Sony SRX-R510DA', '250', 2),
+            (2, 'Ignifugo Norma Europea EN13501-1', 'Sony', 'Sony SRX-R510DB', '250', 2),
+            (3, 'Ignifugo Norma Europea EN13501-1', 'Yamaha', 'Sony SRX-R510DC', '250', 2),
+            (4, 'Ignifugo Norma Europea EN13501-1', 'Yamaha', 'Sony SRX-R510DD', '250', 2),
+            (5, 'Ignifugo Norma Europea EN13501-1', 'Sony', 'Sony SRX-R510DE', '250', 2)
+);
+
+--Sale Cinema "UCI Cinemas Casoria" ID 3
+INSERT INTO cin.sala(numero, schermo, impiantoaudio, impiantovideo, dimensione, cinema)
+(
+    VALUES  (1, 'Ignifugo Norma Europea EN13501-1', 'Bose', 'Sony SRX-R510DA', '250', 3),
+            (2, 'Ignifugo Norma Europea EN13501-1', 'Sony', 'Sony SRX-R510DB', '220', 3),
+            (3, 'Ignifugo Norma Europea EN13501-1', 'Yamaha', 'Sony SRX-R510DR', '110', 3),
+            (4, 'Ignifugo Norma Europea EN13501-1', 'Yamaha', 'Sony SRX-R510D3', '330', 3),
+            (5, 'Ignifugo Norma Europea EN13501-1', 'Sony', 'Sony SRX-R510DG', '210', 3),
+            (6, 'Ignifugo Norma Europea EN13501-1', 'Toshiba', 'Sony SRX-R510DF', '2700', 3)
+);
+
+--Sale Cinema "UCI Cinemas Cinepolis Marcianise" ID 4
+INSERT INTO cin.sala(numero, schermo, impiantoaudio, impiantovideo, dimensione, cinema)
+(
+    VALUES (1,'Ignifugo Norma Europea EN13501-1','Toshiba','Samsung LSP7T',230,4),
+           (2,'Ignifugo Norma Europea EN13501-1','Sony','Samsung LSP8L',120,4),
+           (3, 'Ignifugo Norma Europea EN13501-1','Toshiba','Samsung LSP9T',300,4),
+           (4,'Ignifugo Norma Europea EN13501-1','Yamaha','Samsung LSP9T',240,4),
+           (5,'Ignifugo Norma Europea EN13501-1','Yamaha','Samsung LSP9L',210,4),
+           (6,'Ignifugo Norma Europea EN13501-1','Sony','Samsung LSP9G',150,4),
+           (7,'Ignifugo Norma Europea EN13501-1','Sony','Samsung LSPT10T',350,4)
+);
+
+--Sale Cinema "Metropolitan" ID 5
+INSERT INTO cin.sala(numero, schermo, impiantoaudio, impiantovideo, dimensione, cinema)
+(
+    VALUES (1,'Ignifugo Norma Europea EN13501-1','Bose','Samsung LSP7T',230,5),
+           (2,'Ignifugo Norma Europea EN13501-1','Bose','Samsung LSP8L',100,5),
+           (3, 'Ignifugo Norma Europea EN13501-1','Bose','Samsung LSP9T',300,5)
+);
+
+--Sale Cinema "Multisala Odeon Pisa" ID 6
+INSERT INTO cin.sala(numero, schermo, impiantoaudio, impiantovideo, dimensione, cinema)
+(
+    VALUES  (1, 'Ignifugo Norma Europea EN13501-1', 'Bose', 'Sony SRX-R510DA', 250,6),
+            (2, 'Ignifugo Norma Europea EN13501-1', 'Sony', 'Sony SRX-R510DB', 250,6),
+            (3, 'Ignifugo Norma Europea EN13501-1', 'Yamaha', 'Sony SRX-R510DC', 250,6),
+            (4, 'Ignifugo Norma Europea EN13501-1', 'Yamaha', 'Sony SRX-R510DD', 250,6),
+            (5, 'Ignifugo Norma Europea EN13501-1', 'Sony', 'Sony SRX-R510DE', 250,6),
+            (6, 'Ignifugo Norma Europea EN13501-1', 'Sonos', 'Panasonic PT-RQ13', 21,6),
+            (7, 'Ignifugo Norma Europea EN13501-1', 'Sonos', 'Panasonic PT-RQ13', 32,6),
+            (8, 'Ignifugo Norma Europea EN13501-1', 'Sonos', 'Panasonic PT-RQ16', 430,6),
+            (9, 'Ignifugo Norma Europea EN13501-1', 'Yamaha', 'Panasonic PT-RQ12', 130,6),
+            (10, 'Ignifugo Norma Europea EN13501-1', 'Yamaha', 'Panasonic PT-RQ17', 250,6)
+);
+
+--Sale Cinema "Cinema Teatro San Marco" ID 7
+INSERT INTO cin.sala(numero, schermo, impiantoaudio, impiantovideo, dimensione, cinema)
+(
+    VALUES  (1, 'Ignifugo Norma Europea EN13501-1', 'Bose', 'Panasonic PT-RQ10', 150,7),
+            (2, 'Ignifugo Norma Europea EN13501-1', 'Sony', 'Sony SRX-R510DB', 230,7),
+            (3, 'Ignifugo Norma Europea EN13501-1', 'Sony', 'Sony SRX-R510DZ', 120,7)
+);
