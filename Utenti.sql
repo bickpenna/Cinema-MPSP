1e9654b7c3f97980d799c973eea3d5eb9ab5aa93
Mario Penna <117465087+bickpenna@users.noreply.github.com>
2022-11-05 09:25:48 +0100

    Aggiunto utenti

---
+++ Utenti.sql
@@ -0,0 +1,23 @@
+INSERT INTO cin.cliente(username, password, dataregistrazione)
+VALUES ('bickpenna', '1234567890', '2022-01-19');
+
+INSERT INTO cin.cliente(username, password, dataregistrazione)
+VALUES ('ssimone-p', '0123456789', '2022-02-20');
+
+INSERT INTO cin.cliente(username, password, dataregistrazione)
+VALUES ('Goku', '1234', '2019-08-15');
+
+INSERT INTO cin.cliente(username, password, dataregistrazione)
+VALUES ('vegeta', 'Principe00', '2019-06-20');
+
+INSERT INTO cin.cliente(username, password, dataregistrazione)
+VALUES ('lorenzo-tecchia', 'jetbrains', '2020-02-05');
+
+INSERT INTO cin.cliente(username, password, dataregistrazione)
+VALUES ('homer', 'maggie', '2019-11-11');
+
+INSERT INTO cin.cliente(username, password, dataregistrazione)
+VALUES ('giorgiovanni', 'pirati', '2019-12-08');
+
+INSERT INTO cin.cliente(username, password, dataregistrazione)
+VALUES ('giorgia', 'dux', '2021-10-28');
