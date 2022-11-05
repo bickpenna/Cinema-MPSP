6d3538fd1ab6850f25cc320070a696c28ab929f6
Simone Parente <parentesimone.01@gmail.com>
2022-11-05 16:21:59 +0100

    stessa view
    idattore e idfilm sono vicini

--- Viste/View Recita.sql
+++ Viste/View Recita.sql
@@ -1,3 +1,7 @@
 CREATE VIEW cin.RecitaLabeled
 AS SELECT cin.film.id_film, cin.film.titolo, cin.attore.id_attore, cin.attore.nome, cin.attore.cognome
 FROM (cin.film JOIN cin.recita ON film.id_film = recita.film) JOIN cin.attore ON attore.id_attore=recita.attore;
+
+CREATE VIEW cin.RecitaLabeledv2
+AS SELECT  cin.film.titolo, cin.film.id_film, cin.attore.id_attore, cin.attore.nome, cin.attore.cognome
+FROM (cin.film JOIN cin.recita ON film.id_film = recita.film) JOIN cin.attore ON attore.id_attore=recita.attore;
