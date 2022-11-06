7f0c586d68ab8deb8bdf4146a64e0b98bbf0ad38
Mario Penna <mario.penna00@gmail.com>
2022-11-06 12:14:29 +0100

    Update UnicoFile.sql
    Aggiunto vista ad unico file

--- Dati/UnicoFile.sql
+++ Dati/UnicoFile.sql
@@ -2765,3 +2765,12 @@ INSERT INTO cin.recensione(data, testo, username, film)
 
 
 --FINE POPOLAMENTO RECENSIONI
+
+
+--INIZIO VISTA CON RECITAZIONE, CAST E FILM
+
+CREATE VIEW cin.RecitaLabeledv2
+AS SELECT  cin.film.titolo, cin.film.id_film, cin.attore.id_attore, cin.attore.nome, cin.attore.cognome
+FROM (cin.film JOIN cin.recita ON film.id_film = recita.film) JOIN cin.attore ON attore.id_attore=recita.attore;
+
+--FINE VISTA CON RECITAZIONE, CAST E FILM
